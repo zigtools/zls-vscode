8ca8d856d0fa3cc6f09491b02917d0f5211275c8
SuperAuguste <auguste.rame@gmail.com>
2020-05-14 09:28:07 -0400

    Fixes to README, package.json, .vsix

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 import { workspace, ExtensionContext, commands, OutputChannel, window } from 'vscode';
-import * as WebSocket from 'ws';
 
 import {
   LanguageClient,
@@ -12,7 +11,6 @@ let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
   const zlsPath = workspace.getConfiguration('zigLanguageClient').get('path', '');
-  let socket: WebSocket | null = null;
   
   if (!zlsPath) {
     window.showErrorMessage("Failed to find zls executable! Please specify its path in your settings with `zigLanguageClient.path`.");
@@ -24,36 +22,10 @@ export function activate(context: ExtensionContext) {
     args: []
   };
 
-  commands.registerCommand('zigLanguageClient.startStreaming', () => {
-    socket = new WebSocket(`ws://localhost:7000`);
-  });
-
-  let log = '';
-	const websocketOutputChannel: OutputChannel = {
-		name: 'websocket',
-		// Only append the logs but send them later
-		append(value: string) {
-			log += value;
-			console.log(value);
-		},
-		appendLine(value: string) {
-			log += value;
-			// Don't send logs until WebSocket initialization
-			if (socket && socket.readyState === WebSocket.OPEN) {
-				socket.send(log);
-			}
-			log = '';
-		},
-		clear() {},
-		show() {},
-		hide() {},
-		dispose() {}
-	};
-
   // Options to control the language client
   let clientOptions: LanguageClientOptions = {
     documentSelector: [{ scheme: 'file', language: 'zig' }],
-    outputChannel: websocketOutputChannel //window.createOutputChannel("Zig Language Server")
+    outputChannel: window.createOutputChannel("Zig Language Server")
   };  
 
   // Create the language client and start the client.
